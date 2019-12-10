0e74ddce66d8efa2a8ef29d15068087f4e76618f
shaundramerolla <58342737+shaundramerolla@users.noreply.github.com>
2019-12-10 18:52:42 -0500

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -1,6 +1,6 @@
 <?php
 if($_POST["submit"]) {
-    $recipient="shaundra_perry@my.uri.edu";
+    $recipient="shaundraperry@gmail.com";
     $subject="Form to email message";
     $sender=$_POST["name"];
     $senderEmail=$_POST["email"];
